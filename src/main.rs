bdbde87209df6a00381756e2e05a9975d301bf42
Eduardo Tachotte <etachott@student.42sp.org.br>
2025-08-20 19:28:18 +0200

    Extract args handling to frontend

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
+mod frontend;
 mod lex_error;
 
 use crate::lex_error::LexError;
-use std::env;
 
 fn main() -> Result<(), LexError> {
-    let file_name = env::args()
-        .nth(1)
-        .ok_or_else(|| LexError::FrontendError("File not found!".to_string()))?;
-
-    println!("{}", file_name);
+    let intermediate_representation = frontend::build_ir_from_input();
     Ok(())
 }
