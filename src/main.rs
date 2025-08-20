3d3edd143f384ed639d3d68c2d4c91fded694a60
Eduardo Tachotte <etachott@student.42sp.org.br>
2025-08-20 22:30:41 +0200

    Call frontend in main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod lex_error;
 use crate::lex_error::LexError;
 
 fn main() -> Result<(), LexError> {
-    let intermediate_representation = frontend::build_ir_from_input();
+    let intermediate_representation = frontend::build_ir_from_input()?;
     Ok(())
 }
