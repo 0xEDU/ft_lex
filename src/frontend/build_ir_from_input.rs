0bec516af200e39af08acda7f3fa1bf38595f935
Eduardo Tachotte <etachott@student.42sp.org.br>
2025-08-20 22:30:15 +0200

    Add public frontend interface

--- src/frontend/build_ir_from_input.rs
+++ src/frontend/build_ir_from_input.rs
@@ -1,9 +1,11 @@
 use std::env;
 
-use crate::lex_error::LexError;
+use crate::{frontend::options::Options, lex_error::LexError};
 
 pub fn build_ir_from_input() -> Result<String, LexError> {
-    let arguments: Vec<String> = env::args().skip(1).collect();
+    let arguments = env::args().skip(1);
+    let options = Options::from_arguments(arguments)?;
+    println!("dale");
 
     Ok("dale".to_string())
 }
