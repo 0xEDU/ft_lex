5ff5e1592effdc138b7d9432a0d483507ece6392
Eduardo Tachotte <bfwaend@gmail.com>
2025-09-14 23:02:29 -0300

    Add base tokenizer structure

--- src/modules/input_loader/tokenize_operands.rs
+++ src/modules/input_loader/tokenize_operands.rs
@@ -1,5 +1,21 @@
-pub fn tokenize_operands(operands: Vec<String>) {
+use std::{fs::File, io::{BufRead, BufReader}};
+
+use crate::shared::LexError;
+
+pub fn tokenize_operands(operands: Vec<String>) -> Result<(), LexError> {
     for operand in operands {
-        println!("{}", operand)
+        let file = match File::open(operand) {
+            Ok(f) => f,
+            Err(_) => return Err(LexError::FrontendError("Couldn't open file.".to_string())),
+        };
+
+        let buf_reader = BufReader::new(file);
+        for line in buf_reader.lines() {
+            match line {
+                Ok(line) => println!("{}", line),
+                Err(_) => println!("oops")
+            }
+        }
     }
+    return Ok(());
 }
