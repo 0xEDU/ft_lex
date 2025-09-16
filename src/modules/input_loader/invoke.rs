b9fa64a61577ecf7dbe29a21598d14f8cec92d57
Eduardo Tachotte <bfwaend@gmail.com>
2025-09-16 13:17:52 -0300

    Rename tokenizer call on invoke

--- src/modules/input_loader/invoke.rs
+++ src/modules/input_loader/invoke.rs
@@ -1,14 +1,14 @@
 use std::env;
 
 use crate::{
-    modules::input_loader::{options::Options, tokenize_operands::tokenize_operands},
+    modules::input_loader::{options::Options, tokenizer::Tokenizer},
     shared::LexError,
 };
 
 pub fn invoke() -> Result<String, LexError> {
     let arguments = env::args().skip(1);
     let options = Options::from_arguments(arguments)?;
-    let files = tokenize_operands(options.operands);
+    let files = Tokenizer::new().tokenize_operands(options.operands);
 
     Ok("dale".to_string())
 }
