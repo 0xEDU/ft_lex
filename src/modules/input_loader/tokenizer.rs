e2177de65be723ae548cdab3694a64d46036dfb0
Eduardo Tachotte <bfwaend@gmail.com>
2025-09-20 19:46:50 -0300

    Refactor tokenizer, read file contets into a whole string instead of line by line

--- src/modules/input_loader/tokenizer.rs
+++ src/modules/input_loader/tokenizer.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use std::fs::{self};
 
 use crate::shared::LexError;
 
@@ -14,95 +11,35 @@ enum TokenizerState {
 
 pub struct Tokenizer {
     state: TokenizerState,
-    is_inside_code_block: bool,
 }
 
 impl Tokenizer {
     pub fn new() -> Self {
         Tokenizer {
             state: TokenizerState::Definitions,
-            is_inside_code_block: false,
         }
     }
 
-    fn next(&mut self) {
+    fn next_state(&mut self) {
         match self.state {
-            TokenizerState::Definitions => {
-                self.state = TokenizerState::Rules;
-            }
+            TokenizerState::Definitions => self.state = TokenizerState::Rules,
             TokenizerState::Rules => self.state = TokenizerState::UserSubroutines,
             _ => {}
         }
     }
 
-    fn tokenize_definitions(&mut self, line: String) {
-        if self.is_inside_code_block {
-            if line.starts_with("%}") {
-                println!("code block end");
-                self.is_inside_code_block = false;
-                return;
-            }
-            println!("line inside code block!");
-            return;
-        }
-
-        let mut iter = line.chars();
-        // %(alpha) -> split by white space, then check first element
-        // in vector, do pattern matching for valid patterns like 
-        // %p or %pointer
-        match iter.next() {
-            Some(' ' | '\t') => {
-                println!("found a line of C code")
-            }
-            Some('%') => match iter.next() {
-                Some('{') => {
-                    println!("code block start");
-                    self.is_inside_code_block = true;
-                }
-                Some(c) => {
-                    println!("start condition")
-                } // Branch out valid start conditions
-                None => {
-                    println!("syntax error")
-                }
-            },
-            Some(_) => {
-                println!("name defition")
-            }
-            None => {
-                println!("blank line, shouldn't reach this")
-            }
-        }
+    fn tokenize_definitions(&mut self) {
     }
 
     pub fn tokenize_operands(&mut self, operands: Vec<String>) -> Result<(), LexError> {
         for operand in operands {
-            let file = File::open(operand)?;
-            let buf_reader = BufReader::new(file);
-            let mut current_line = 1;
+            let content = fs::read_to_string(operand)?;
 
-            for line in buf_reader.lines() {
-                let line = line?;
-                if line.is_empty() {
-                    current_line += 1;
-                    continue;
-                }
-
-                if line == "%%" && !self.is_inside_code_block {
-                    self.next();
-                }
-
-                print!("current line: {}: ", current_line);
-                match self.state {
-                    TokenizerState::Definitions => self.tokenize_definitions(line),
-                    TokenizerState::Rules => {
-                        println!("")
-                    }
-                    TokenizerState::UserSubroutines => {
-                        println!("")
-                    }
-                }
-                current_line += 1;
+            match self.state {
+                TokenizerState::Definitions => self.tokenize_definitions(),
+                TokenizerState::Rules => println!("rules"),
+                TokenizerState::UserSubroutines => println!("user subroutes")
+                
             }
         }
         return Ok(());
