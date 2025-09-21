b617617e51c74696409a1d64fc6e3f6411f65c54
Eduardo Tachotte <bfwaend@gmail.com>
2025-09-20 23:12:03 -0300

    Add a Cursor implementation, looking more robust now

--- src/modules/input_loader/tokenizer.rs
+++ src/modules/input_loader/tokenizer.rs
@@ -2,6 +2,44 @@ use std::fs::{self};
 
 use crate::shared::LexError;
 
+struct Cursor<'a> {
+    content: &'a [u8],
+    current: usize,
+}
+
+impl<'a> Cursor<'a> {
+    pub fn new(content: &'a str) -> Self {
+        let content: &'a [u8] = content.as_bytes();
+        Cursor { 
+            content,
+            current: 0,
+         }
+    }
+
+    pub fn peek(&self) -> Option<&u8> {
+        self.content.get(self.current)
+    }
+
+    pub fn is_at_end(&self) -> bool {
+        self.current >= self.content.len()
+    }
+
+    pub fn advance_one(&mut self) -> Option<&u8> {
+        self.advance(1)
+    }
+
+    pub fn advance(&mut self, quantity: usize) -> Option<&u8> {
+        let byte = self.content.get(self.current);
+        self.current += quantity;
+        return byte;
+    }
+
+    pub fn is_at_str(&self, s: &str) -> bool {
+        let end = self.current + s.len();
+        end <= self.content.len() && &self.content[self.current..end] == s.as_bytes()
+    }
+}
+
 #[derive(Debug)]
 enum TokenizerState {
     Definitions,
@@ -28,19 +66,35 @@ impl Tokenizer {
         }
     }
 
-    fn tokenize_definitions(&mut self) {
-    }
+    fn tokenize_operand(&mut self, operand: String) -> Result<(), LexError> {
+        let content = fs::read_to_string(operand)?;
+        let mut cursor = Cursor::new(&content);
+        let state = TokenizerState::Rules;
 
-    pub fn tokenize_operands(&mut self, operands: Vec<String>) -> Result<(), LexError> {
-        for operand in operands {
-            let content = fs::read_to_string(operand)?;
+        while !cursor.is_at_end() {
+            if matches!(cursor.peek(), Some(b'\n')) {
+                
+            }
 
-            match self.state {
-                TokenizerState::Definitions => self.tokenize_definitions(),
+            match state {
+                TokenizerState::Definitions => {
+                    if cursor.is_at_str("%%") {
+                        println!("found a section delimiter");
+                        cursor.advance(2);
+                        continue;
+                    }
+                },
                 TokenizerState::Rules => println!("rules"),
                 TokenizerState::UserSubroutines => println!("user subroutes")
                 
             }
+        }
+        return Ok(());
+    }
+
+    pub fn tokenize_operands(&mut self, operands: Vec<String>) -> Result<(), LexError> {
+        for operand in operands {
+            self.tokenize_operand(operand)?;
         }
         return Ok(());
     }
