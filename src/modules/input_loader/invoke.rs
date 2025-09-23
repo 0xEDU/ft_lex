6e33768f1575e7ee9b9554500b4999e2c85f1f27
Eduardo Tachotte <bfwaend@gmail.com>
2025-09-22 21:16:15 -0300

    Update function call on input_loader invoke

--- src/modules/input_loader/invoke.rs
+++ src/modules/input_loader/invoke.rs
@@ -1,14 +1,14 @@
 use std::env;
 
 use crate::{
-    modules::input_loader::{options::Options, tokenizer::Tokenizer},
+    modules::input_loader::{options::Options, tokenizer::tokenize_operands},
     shared::LexError,
 };
 
 pub fn invoke() -> Result<String, LexError> {
     let arguments = env::args().skip(1);
     let options = Options::from_arguments(arguments)?;
-    let files = Tokenizer::new().tokenize_operands(options.operands);
+    let files = tokenize_operands(options.operands);
 
     Ok("dale".to_string())
 }
