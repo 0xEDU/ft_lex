f20dd6b085cfb7dd7ccd90ff242f43004fe80fed
edu <bfwaend@gmail.com>
2025-10-03 22:28:17 -0300

    Increment tokens enum

--- src/modules/input_loader/mod.rs
+++ src/modules/input_loader/mod.rs
@@ -2,5 +2,6 @@ mod cursor;
 pub mod invoke;
 mod options;
 mod tokenize_operands;
+mod tokens;
 
 pub use invoke::invoke;
--- src/modules/input_loader/tokens.rs
+++ src/modules/input_loader/tokens.rs
@@ -1,3 +1,17 @@
+#[derive(Debug, PartialEq)]
+pub enum ScannerStorageKind {
+    Array,
+    Pointer
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Token {
-    CodeBlock(Vec<u8>)
+    CodeLine(Vec<u8>),
+    CodeBlock(Vec<u8>),
+    StartConditionInclusive(Vec<u8>),
+    StartConditionExclusive(Vec<u8>),
+    ScannerStorage(ScannerStorageKind),
+    MacroDefinition(Vec<u8>),
+    Rule(Vec<u8>),
+    UserSubroutine(Vec<u8>),
 }
