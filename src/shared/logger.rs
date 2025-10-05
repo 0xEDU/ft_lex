31f5b1ce37e286a327b004fb2fc8fbafdea07bad
Eduardo Tachotte <bfwaend@gmail.com>
2025-10-05 13:55:58 -0300

    Add logger for parser

--- src/shared/logger.rs
+++ src/shared/logger.rs
@@ -1,3 +1,7 @@
 pub fn tokenizer_error(file: &str, line: usize, msg: &str) {
     println!("{}:{} {}", file, line, msg)
 }
+
+pub fn parser_error(msg: &str) {
+    println!("invalid condition: {}", msg)
+}
